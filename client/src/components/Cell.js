e69fb2ea48a78e9ea81272585d6b22f8154a0ca3
Simon Amarasingham <simon@dsonic.com>
2019-07-30 22:02:12 -0700

    added Quill

--- client/src/components/Cell.js
+++ client/src/components/Cell.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
+import ReactQuill from 'react-quill';
 
 class Cell extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { text: this.props.content };
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(value) {
+		this.setState({ text: value });
+	}
+
 	createCellContent() {
-		return <div>*** {this.props.content} ***</div>;
+		return (
+			<div>
+				<ReactQuill theme="snow" value={this.state.text} onChange={this.handleChange} />
+			</div>
+		);
 	}
 
 	render() {
-		return (
-			<div className="cell dark-dark-blue" contentEditable="true">
-				{this.createCellContent()}
-			</div>
-		);
+		// top level div used to have this: contentEditable="true"
+		return <div className="cell dark-dark-blue">{this.createCellContent()}</div>;
 	}
 }
 
