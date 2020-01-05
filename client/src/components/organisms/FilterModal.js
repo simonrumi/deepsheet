c12af57df1c4595f28f0cc675b5c1832423f5f60
Simon Amarasingham <simon@dsonic.com>
2020-01-04 21:38:55 -0800

    filter modal positioned properly

--- client/src/components/organisms/FilterModal.js
+++ client/src/components/organisms/FilterModal.js
@@ -7,77 +7,72 @@ import SortOptions from '../molecules/SortOptions';
 import FilterOptions from '../molecules/FilterOptions';
 
 class FilterModal extends Component {
-   constructor(props) {
-      super(props);
-      this.editFilter = this.editFilter.bind(this);
-   }
+	constructor(props) {
+		super(props);
+		this.editFilter = this.editFilter.bind(this);
+	}
 
-   render() {
-      if (this.props.showFilterModal) {
-         return (
-            <form
-               className="z-10 w-1/2 md:w-1/3 border border-solid border-grey-blue px-2 py-2"
-               onSubmit={this.props.handleSubmit(this.editFilter)}
-            >
-               <SortOptions
-                  classes=""
-                  onClickAtoZ={() => alert('onClickAtoZ button clicked')}
-                  onClickZtoA={() => alert('onClickZtoA button clicked')}
-               />
-               <FilterOptions />
-               <div className="flex items-center justify-around px-2 py-1">
-                  <Button
-                     buttonType="submit"
-                     classes=""
-                     label="OK"
-                     disabled={this.props.pristine || this.props.submitting}
-                  />
-                  <Button
-                     buttonType="cancel"
-                     classes=""
-                     onClickFn={this.props.reset}
-                     label="Cancel"
-                  />
-               </div>
-            </form>
-         );
-      }
-      return null;
-   }
+	render() {
+		if (this.props.showFilterModal) {
+			return (
+				<form
+					className="fixed top-1/3 left-1/3 w-1/2 md:w-1/3 border border-solid border-grey-blue bg-white shadow-lg px-2 py-2"
+					onSubmit={this.props.handleSubmit(this.editFilter)}
+				>
+					<SortOptions
+						classes=""
+						onClickAtoZ={() => alert('onClickAtoZ button clicked')}
+						onClickZtoA={() => alert('onClickZtoA button clicked')}
+					/>
+					<FilterOptions />
+					<div className="flex items-center justify-around px-2 py-1">
+						<Button
+							buttonType="submit"
+							classes=""
+							label="OK"
+							disabled={this.props.pristine || this.props.submitting}
+						/>
+						<Button buttonType="cancel" classes="" onClickFn={this.props.reset} label="Cancel" />
+					</div>
+				</form>
+			);
+		}
+		return null;
+	}
 
-   editFilter = formValues => {
-      this.props.updatedFilter({
-         filterExpression: formValues.filterExpression,
-         caseSensitive: formValues.caseSensitiveCheckbox,
-         regex: formValues.regexCheckbox,
-         showFilterModal: false,
-         rowIndex: this.props.rowIndex,
-         colIndex: this.props.colIndex,
-      });
-   };
+	editFilter = formValues => {
+		this.props.updatedFilter({
+			filterExpression: formValues.filterExpression,
+			caseSensitive: formValues.caseSensitiveCheckbox,
+			regex: formValues.regexCheckbox,
+			showFilterModal: false,
+			rowIndex: this.props.rowIndex,
+			colIndex: this.props.colIndex,
+		});
+	};
 }
 
 const validateForm = formValues => {
-   const errors = {};
-   // add error checking here, object keys should be the same as the Field names
-   return errors;
+	const errors = {};
+	// add error checking here, object keys should be the same as the Field names
+	return errors;
 };
 
 const filterForm = reduxForm({
-   form: 'filterForm', // a name for the form that shows up in the redux store
-   validate: validateForm,
+	form: 'filterForm', // a name for the form that shows up in the redux store
+	validate: validateForm,
 })(FilterModal);
 
 function mapStateToProps(state, ownProps) {
-   return {
-      sheet: state.sheet, // might not really need this
-      showFilterModal: state.filterModal.showFilterModal,
-      rowIndex: state.filterModal.rowIndex,
-      colIndex: state.filterModal.colIndex,
-   };
+	return {
+		sheet: state.sheet, // might not really need this
+		showFilterModal: state.filterModal.showFilterModal,
+		rowIndex: state.filterModal.rowIndex,
+		colIndex: state.filterModal.colIndex,
+	};
 }
 
 export default connect(
-   mapStateToProps,
-   { updatedFilter }
+	mapStateToProps,
+	{ updatedFilter }
 )(filterForm);
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,28 +1,35 @@
 module.exports = {
-   theme: {
-      extend: {
-         colors: {
-            'dark-dark-blue': '#04191c',
-            'grey-blue': '#a5cacf',
-            'light-light-blue': '#c7f8ff',
-            'vibrant-blue': '#3ed2e5',
-            'subdued-blue': '#32a8b8',
-            'burnt-orange': '#B87B32',
-            'vibrant-burnt-orange': '#E69A3E',
-         },
-      },
-      flex: {
-         '2': '2 2 0%',
-      },
-   },
-   variants: {},
-   plugins: [
-      function({ addVariant, e }) {
-         addVariant('checked', ({ modifySelectors, separator }) => {
-            modifySelectors(({ className }) => {
-               return `.${e(`checked${separator}${className}`)}:checked`;
-            });
-         });
-      },
-   ],
+	theme: {
+		extend: {
+			colors: {
+				'dark-dark-blue': '#04191c',
+				'grey-blue': '#a5cacf',
+				'light-light-blue': '#c7f8ff',
+				'vibrant-blue': '#3ed2e5',
+				'subdued-blue': '#32a8b8',
+				'burnt-orange': '#B87B32',
+				'vibrant-burnt-orange': '#E69A3E',
+			},
+			inset: {
+				'1/4': '25%',
+				'1/3': '33%',
+				'1/2': '50%',
+				'2/3': '67%',
+				'3/4': '75%',
+			},
+		},
+		flex: {
+			'2': '2 2 0%',
+		},
+	},
+	variants: {},
+	plugins: [
+		function({ addVariant, e }) {
+			addVariant('checked', ({ modifySelectors, separator }) => {
+				modifySelectors(({ className }) => {
+					return `.${e(`checked${separator}${className}`)}:checked`;
+				});
+			});
+		},
+	],
 };
