abe4ed3cff48c8e366f460f6e2508813fb53adba
Simon Amarasingham <simon@dsonic.com>
2020-01-20 17:43:21 -0800

    got filters working (for the 2nd time)

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,117 +2,114 @@
 import * as R from 'ramda';
 import managedStore from '../store';
 import {
-   FETCHED_SHEET,
-   UPDATE_EDITOR,
-   UPDATED_CELL_,
-   UPDATED_CELL_KEYS,
-   SET_EDITOR_REF,
-   UPDATED_TITLE,
-   SET_EDITING_TITLE,
-   UPDATED_SHEET_ID,
-   TOGGLED_SHOW_FILTER_MODAL,
-   UPDATED_FILTER,
-   UPDATED_COLUMN_FILTERS,
-   UPDATED_ROW_FILTERS,
-   HIDE_FILTERED,
-   UPDATED_COLUMN_VISIBILITY,
-   UPDATED_ROW_VISIBILITY,
+	FETCHED_SHEET,
+	UPDATE_EDITOR,
+	UPDATED_CELL_,
+	UPDATED_CELL_KEYS,
+	SET_EDITOR_REF,
+	UPDATED_TITLE,
+	SET_EDITING_TITLE,
+	UPDATED_SHEET_ID,
+	TOGGLED_SHOW_FILTER_MODAL,
+	UPDATED_FILTER,
+	UPDATED_COLUMN_FILTERS,
+	UPDATED_ROW_FILTERS,
+	HIDE_FILTERED,
+	UPDATED_COLUMN_VISIBILITY,
+	UPDATED_ROW_VISIBILITY,
 } from './types';
 
 export const fetchedSheet = sheet => {
-   managedStore.store.dispatch({ type: FETCHED_SHEET, payload: sheet });
+	managedStore.store.dispatch({ type: FETCHED_SHEET, payload: sheet });
 };
 
 export const updatedSheetId = sheetId => {
-   managedStore.store.dispatch({ type: UPDATED_SHEET_ID, payload: sheetId });
+	managedStore.store.dispatch({ type: UPDATED_SHEET_ID, payload: sheetId });
 };
 
 export const updatedHiddenColumns = hiddenColumns => {
-   managedStore.store.dispatch({
-      type: UPDATED_COLUMN_VISIBILITY,
-      payload: hiddenColumns,
-   });
+	managedStore.store.dispatch({
+		type: UPDATED_COLUMN_VISIBILITY,
+		payload: hiddenColumns,
+	});
 };
 
 export const updatedHiddenRows = hiddenRows => {
-   managedStore.store.dispatch({
-      type: UPDATED_ROW_VISIBILITY,
-      payload: hiddenRows,
-   });
+	managedStore.store.dispatch({
+		type: UPDATED_ROW_VISIBILITY,
+		payload: hiddenRows,
+	});
 };
 
 export const updatedColumnFilters = columnFilters => {
-   managedStore.store.dispatch({
-      type: UPDATED_COLUMN_FILTERS,
-      payload: columnFilters,
-   });
+	managedStore.store.dispatch({
+		type: UPDATED_COLUMN_FILTERS,
+		payload: columnFilters,
+	});
 };
 
 export const updatedRowFilters = rowFilters => {
-   managedStore.store.dispatch({
-      type: UPDATED_ROW_FILTERS,
-      payload: rowFilters,
-   });
+	managedStore.store.dispatch({
+		type: UPDATED_ROW_FILTERS,
+		payload: rowFilters,
+	});
 };
 
 export const updateEditor = cellBeingEdited => {
-   managedStore.store.dispatch({
-      type: UPDATE_EDITOR,
-      payload: cellBeingEdited,
-   });
+	managedStore.store.dispatch({
+		type: UPDATE_EDITOR,
+		payload: cellBeingEdited,
+	});
 };
 
 export const updatedCellBeingEdited = cell => {
-   const updateCellType = UPDATED_CELL_ + cell.row + '_' + cell.column;
-   managedStore.store.dispatch({ type: updateCellType, payload: cell.content });
+	const updateCellType = UPDATED_CELL_ + cell.row + '_' + cell.column;
+	managedStore.store.dispatch({ type: updateCellType, payload: cell.content });
 };
 
 export const updatedCell = cell => {
-   if (!cell || !cell.content) {
-      console.log(
-         'WARNING: updateCell could not create an action. It received',
-         cell
-      );
-      return;
-   }
-   const updateCellType = UPDATED_CELL_ + cell.row + '_' + cell.column;
-   managedStore.store.dispatch({ type: updateCellType, payload: cell }); // was payload: cell.content });
+	if (!cell || !cell.content) {
+		console.log('WARNING: updateCell could not create an action. It received', cell);
+		return;
+	}
+	const updateCellType = UPDATED_CELL_ + cell.row + '_' + cell.column;
+	managedStore.store.dispatch({ type: updateCellType, payload: cell }); // was payload: cell.content });
 };
 
 export const updatedCellKeys = keys => {
-   managedStore.store.dispatch({ type: UPDATED_CELL_KEYS, payload: keys });
+	managedStore.store.dispatch({ type: UPDATED_CELL_KEYS, payload: keys });
 };
 
 export const setEditorRef = editorRef => {
-   managedStore.store.dispatch({ type: SET_EDITOR_REF, payload: editorRef });
+	managedStore.store.dispatch({ type: SET_EDITOR_REF, payload: editorRef });
 };
 
 export const updatedTitle = titleData => {
-   managedStore.store.dispatch({ type: UPDATED_TITLE, payload: titleData });
+	managedStore.store.dispatch({ type: UPDATED_TITLE, payload: titleData });
 };
 
 export const setEditingTitle = isEditingTitle => {
-   managedStore.store.dispatch({
-      type: SET_EDITING_TITLE,
-      payload: isEditingTitle,
-   });
+	managedStore.store.dispatch({
+		type: SET_EDITING_TITLE,
+		payload: isEditingTitle,
+	});
 };
 
 export const toggledShowFilterModal = (rowIndex, colIndex) => {
-   const showModal = !R.isNil(rowIndex) || !R.isNil(colIndex);
-   managedStore.store.dispatch({
-      type: TOGGLED_SHOW_FILTER_MODAL,
-      payload: { showModal, rowIndex, colIndex },
-   });
+	const showModal = !R.isNil(rowIndex) || !R.isNil(colIndex);
+	managedStore.store.dispatch({
+		type: TOGGLED_SHOW_FILTER_MODAL,
+		payload: { showModal, rowIndex, colIndex },
+	});
 };
 
 export const updatedFilter = filterOptions => {
-   managedStore.store.dispatch({
-      type: UPDATED_FILTER,
-      payload: filterOptions,
-   });
-   managedStore.store.dispatch({
-      type: HIDE_FILTERED,
-      payload: filterOptions,
-   });
+	managedStore.store.dispatch({
+		type: UPDATED_FILTER,
+		payload: filterOptions,
+	});
+	managedStore.store.dispatch({
+		type: HIDE_FILTERED,
+		payload: filterOptions,
+	});
 };
--- client/src/middleware/filterSheet.js
+++ client/src/middleware/filterSheet.js
@@ -1,246 +1,200 @@
 /*
-See notes in getVisibilityForCellsInAxis
-
-BUT MAYBE first refactor store so that columnVisibility, rowVisibility, columnFilters and rowFilters
+TODO: MAYBE refactor store so that columnVisibility, rowVisibility, columnFilters and rowFilters
 are pulled out from sheet and put at the top level of the store (sheet data stays as is)
-
-Then make actions to update each of those
-
-Then work out some string of actions like
-dispatch UPDATED_FILTER when user is typing in filter text input
-when OK button is clicked in filter modal
-tirgger updatedColumnFilters or updatedRowFilters with the full sheet data
-Then dispatch HIDE_FILTERED to trigger the code here
-
 */
 
-import * as R from 'ramda';
-import {
-   UPDATED_COLUMN_VISIBILITY,
-   UPDATED_ROW_VISIBILITY,
-   HIDE_FILTERED,
-} from '../actions/types';
-import { updatedColumnFilters, updatedRowFilters } from '../actions';
-import {
-   extractRowColFromCellKey,
-   ROW_AXIS,
-   COLUMN_AXIS,
-   nothing,
-} from '../helpers';
-import {
-   getVisibilityForAxis,
-   getTotalForAxis,
-   replaceFilterEntry,
-   getFilterName,
-} from '../helpers/visibilityHelpers';
-//import * as RWrap from '../helpers/ramdaWrappers';
-
-/* these are used by multiple functions below */
-const getAxis = data =>
-   R.ifElse(R.isNil, () => COLUMN_AXIS, () => ROW_AXIS)(data.rowIndex);
-
-const getOppositeAxis = data =>
-   R.ifElse(R.isNil, () => ROW_AXIS, () => COLUMN_AXIS)(data.rowIndex);
-
-const getOtherAxis = axis => (axis === ROW_AXIS ? COLUMN_AXIS : ROW_AXIS);
-
-const getAxisVisibilityName = axis => R.concat(axis, 'Visibility'); // will be "rowVisibility" or "columnVisibility"
-
-const getActionTypeByAxis = axis =>
-   axis === ROW_AXIS ? UPDATED_ROW_VISIBILITY : UPDATED_COLUMN_VISIBILITY;
-
-const checkEachItemAgainstFilter = (axisName, sheet) => {
-   const axisFilters = sheet[R.concat(axisName, 'Filters')];
-   console.log('axisName', axisName);
-   console.log('axisFilters', axisFilters);
-   return R.map(filter => {
-      console.log('filter,', filter);
-   }, axisFilters);
-};
-
-// TODO - building this
-// for each axis <filterWholeSheet>
-// 	** don't do?:  if axisFilters are not empty <applyAllFiltersInAxis>
-// 		for each oppositeAxis Item <filterAllItemsInOtherAxis>
-// 			start by setting the axis visibility to true (unhidden) <??>
-// 			for each filter in axisFilters <setVisibilityForOtherAxisItem>
-// 				is the cell in the opposite axis hidden by the axis filter? <isCellHiddenByFilter>
-// 					if yes - hide the oppositeAxis and stop
-//
 // TODO NEXT :
 // for each cell
 // 	cell visibility = row visibility && column visibility
 //
 
+import * as R from 'ramda';
+import { UPDATED_COLUMN_VISIBILITY, UPDATED_ROW_VISIBILITY, HIDE_FILTERED } from '../actions/types';
+import { updatedColumnFilters, updatedRowFilters } from '../actions';
+import { ROW_AXIS, COLUMN_AXIS } from '../helpers';
+import { getTotalForAxis } from '../helpers/visibilityHelpers';
+import { updatedCell } from '../actions';
+
+/* these are used by multiple functions below */
+const getAxis = data => R.ifElse(R.isNil, () => COLUMN_AXIS, () => ROW_AXIS)(data.rowIndex);
+
+const getOppositeAxis = data => R.ifElse(R.isNil, () => ROW_AXIS, () => COLUMN_AXIS)(data.rowIndex);
+
+const getOtherAxis = axis => (axis === ROW_AXIS ? COLUMN_AXIS : ROW_AXIS);
+
+const getAxisVisibilityName = axis => R.concat(axis, 'Visibility'); // will be "rowVisibility" or "columnVisibility"
+
+const getActionTypeByAxis = axis => (axis === ROW_AXIS ? UPDATED_ROW_VISIBILITY : UPDATED_COLUMN_VISIBILITY);
+
+const getStateFromData = data => R.path(['store', 'getState'], data)();
+
+const getSheetFromData = R.pipe(
+	getStateFromData,
+	R.prop('sheet')
+);
+
 const getFilters = (axisName, sheet) => sheet[R.concat(axisName, 'Filters')]; // helper for a couple of fns below
 
-//is the cell in the opposite axis hidden by the axis filter?
-const isCellHiddenByFilter = (cell, filter) => {
-   // ******** TODO there should be some centralized function to compare cell content to a filter and return true or false
-   // it should take into account the regex and case sensitive flags
-   const regex = new RegExp(filter.filterExpression);
-   return regex.test(cell.content);
+// TODO there should be some centralized function to compare cell content to a filter and return true or false
+// it should take into account the regex and case sensitive flags
+const isCellShownByFilter = (cell, filter) => {
+	const regex = new RegExp(filter.filterExpression);
+	return regex.test(cell.content);
 };
 
 const createCellKey = (data, axis, itemIndex, otherAxisIndex) => {
-   const rowIndex = axis === ROW_AXIS ? itemIndex : otherAxisIndex;
-   const colIndex = axis === COLUMN_AXIS ? itemIndex : otherAxisIndex;
-   const cellKey = 'cell_' + rowIndex + '_' + colIndex;
-   console.log('createCellKey, axis = ', axis);
-   console.log('createCellKey, cellKey = ', cellKey);
-   return cellKey; // TODO make createCellKey(rowIndex, colIndex) and put in cellHelpers
+	const rowIndex = axis === ROW_AXIS ? itemIndex : otherAxisIndex;
+	const colIndex = axis === COLUMN_AXIS ? itemIndex : otherAxisIndex;
+	const cellKey = 'cell_' + rowIndex + '_' + colIndex;
+	return cellKey; // TODO make createCellKey(rowIndex, colIndex) and put in cellHelpers
 };
 
-const getVisibilityForCellsInAxis = (data, axis, indexOfOtherAxis) => {
-   console.log(
-      'getVisibilityForCellsInAxis, data =',
-      data,
-      ', axis =',
-      axis,
-      'indexOfOtherAxis =',
-      indexOfOtherAxis
-   );
+// TODO make this more functional
+const getVisibilityForCellsInAxisItem = (data, axis, itemIndex) => {
+	const filters = getFilters(getOtherAxis(axis), getSheetFromData(data));
 
-   // BUG TODO: getFilters now returns correct object with filters for axis in it....check what is happening next
-   // BUG TODO: getVisibilityForCellsInAxis, createCellKey and createAxisVisibilityUpdateAction are being called twice
-   // for each cell (same results though)
-   console.log(
-      'getVisibilityForCellsInAxis getFilters(axis, data.store.getState().sheet) will give',
-      getFilters(axis, data.store.getState().sheet)
-   );
+	// if we have no filters that is the same as saying every cell should be visible,
+	// so we can return an object saying just one column/row is visible, and this will be reduced in getAxisVisibilityValue
+	// to a single true value for the whole row or column
+	if (R.isNil(filters)) {
+		return { 0: true }; //
+	}
 
-   const sheet = data.store.getState().sheet;
-
-   // **** TODO: remember that data.filterExpression contains new filter data for the clicked axis item
-
-   return R.mapObjIndexed((filter, filterIndex, axisFilters) => {
-      const cellKey = createCellKey(data, axis, indexOfOtherAxis, filterIndex);
-      const cell = data.store.getState()[cellKey];
-      return !isCellHiddenByFilter(cell, filter);
-   }, getFilters(axis, data.store.getState().sheet));
+	// note R.mapObjIndexed sends its 1st argument, a function, the parameters (value, key, obj), but we're not using the obj
+	return R.mapObjIndexed((filter, filterIndex) => {
+		const cellKey = createCellKey(data, axis, itemIndex, filterIndex);
+		const cell = R.prop(cellKey, getStateFromData(data));
+		return isCellShownByFilter(cell, filter);
+	}, getFilters(getOtherAxis(axis), getSheetFromData(data)));
 };
 
 // receives (data, axis, itemIndex)
 const getAxisVisibilityValue = R.pipe(
-   getVisibilityForCellsInAxis,
-   R.values,
-   R.reduce((accumulator, isVisible) => accumulator && isVisible, true)
+	getVisibilityForCellsInAxisItem,
+	R.values,
+	R.reduce((accumulator, isVisible) => accumulator && isVisible, true)
 );
 
-// receives (data, axis, itemIndex)
-const getAxisVisibilityObj = R.converge(R.prop, [
-   R.flip(getAxisVisibilityName),
-   R.path(['storeState', 'sheet']),
-]);
+const getAxisVisibilityObj = R.converge(R.prop, [R.flip(getAxisVisibilityName), getSheetFromData]); // receives (data, axis, itemIndex)
 
 // receives (data, axis, itemIndex)
-const getNewVisibilityForAxis = R.converge(R.assoc, [
-   (data, axis, itemIndex) => itemIndex,
-   getAxisVisibilityValue,
-   getAxisVisibilityObj,
+const getNewVisibilityForAxisItem = R.converge(R.assoc, [
+	(data, axis, itemIndex) => itemIndex,
+	getAxisVisibilityValue,
+	getAxisVisibilityObj,
 ]);
 
 const actionTypeLens = R.lens(R.prop('type'), R.assoc('type'));
 const actionPayloadLens = R.lens(R.prop('payload'), R.assoc('payload'));
 const makeAction = (type, payload) =>
-   R.pipe(
-      R.set(actionTypeLens, type),
-      R.set(actionPayloadLens, payload)
-   )({});
+	R.pipe(
+		R.set(actionTypeLens, type),
+		R.set(actionPayloadLens, payload)
+	)({});
 
 const createAxisVisibilityUpdateAction = R.converge(makeAction, [
-   R.flip(getActionTypeByAxis),
-   getNewVisibilityForAxis,
+	R.flip(getActionTypeByAxis),
+	getNewVisibilityForAxisItem,
 ]);
 
-// for each filter in axisFilters
-// TODO remove console logs
-const setVisibilityForOtherAxisItem = R.curry((data, axis, itemIndex) => {
-   console.log('setVisibilityForOtherAxisItem', itemIndex);
-   console.log(
-      'createAxisVisibilityUpdateAction(data, getOtherAxis(axis), itemIndex)',
-      createAxisVisibilityUpdateAction(data, getOtherAxis(axis), itemIndex)
-   );
-   R.pipe(
-      getOtherAxis,
-      createAxisVisibilityUpdateAction(data, R.__, itemIndex),
-      data.store.dispatch
-   )(axis);
-});
+const setVisibilityForAxisItem = R.curry((data, axis, itemIndex) =>
+	R.pipe(
+		createAxisVisibilityUpdateAction(data, R.__, itemIndex),
+		data.store.dispatch
+	)(axis)
+);
 
-// TODO remove console logs
-// for each oppositeAxis Item
 const filterAllItemsInOtherAxis = R.curry((data, axis) => {
-   console.log('filterAllItemsInOtherAxis, axis = ', axis);
-   console.log(
-      'getTotalForAxis(axis, data.store.getState().sheet)',
-      getTotalForAxis(axis, data.store.getState().sheet)
-   );
-   R.times(
-      setVisibilityForOtherAxisItem(data, axis),
-      getTotalForAxis(axis, data.store.getState().sheet)
-   );
+	const otherAxis = getOtherAxis(axis);
+	R.times(setVisibilityForAxisItem(data, otherAxis), getTotalForAxis(otherAxis, getSheetFromData(data)));
 });
 
-// if axisFilters are not empty - QQQQ skipping this for now
-const applyAllFiltersInAxis = R.curry((data, axis) => {
-   console.log('applyAllFiltersInAxis, data = ', data, 'axis = ', axis);
-   return R.pipe(
-      getFilters,
-      R.isEmpty
-   )(axis, data.store.getState().sheet)
-      ? nothing()
-      : filterAllItemsInOtherAxis(data);
-});
+const getCellVisibilityForAxis = R.curry((cell, axis) =>
+	R.pipe(
+		getSheetFromData,
+		R.path([getAxisVisibilityName(axis), R.prop(axis, cell)])
+	)
+);
+
+const getCellVisibilityFnsFromCell = cell => R.map(getCellVisibilityForAxis(cell), [ROW_AXIS, COLUMN_AXIS]);
+
+const getCellFromData = R.curry((data, cellKey) =>
+	R.pipe(
+		getStateFromData,
+		R.prop(cellKey)
+	)(data)
+);
+
+const getCellVisibilityFns = R.pipe(
+	getCellFromData,
+	getCellVisibilityFnsFromCell
+);
+
+const setVisibilityForCell = (data, cellKey) => {
+	const cellVisibility = R.reduce(
+		(accumulator, visibilityFn) => visibilityFn(data) && accumulator,
+		true,
+		getCellVisibilityFns(data, cellKey)
+	);
+	const cell = getCellFromData(data, cellKey);
+	updatedCell({ ...cell, visible: cellVisibility });
+};
+
+const getCellKeysFromData = R.pipe(
+	getStateFromData,
+	R.prop('cellKeys')
+);
+
+const filterCells = data => {
+	const cellKeys = getCellKeysFromData(data);
+	R.map(cellKey => {
+		return setVisibilityForCell(data, cellKey);
+	}, cellKeys);
+};
 
 // TODO this should really be a pipe. Functions above should output data to go to next function
-// for each axis
-const filterWholeSheet = data =>
-   R.map(filterAllItemsInOtherAxis(data), [
-      getAxis(data),
-      getOppositeAxis(data),
-   ]);
-//R.map(applyAllFiltersInAxis(data), [getAxis(data), getOppositeAxis(data)]);
+const filterAxes = data => {
+	R.map(filterAllItemsInOtherAxis(data), [getAxis(data), getOppositeAxis(data)]);
+	return data;
+};
 
 /* getDataFromActionAndStore - creates a data object for passing to subsequent functions in hideFiltered's pipe */
-const getDataFromActionAndStore = (actionData, store) =>
-   R.mergeAll([actionData, { store }, { storeState: store.getState() }]);
+const getDataFromActionAndStore = (actionData, store) => R.mergeAll([actionData, { store }]);
 
-// TODO make more functional and remove console logs
+// TODO make more functional
 const addNewFilter = data => {
-   const filterName = getFilterName(data);
-   const sheet = data.store.getState().sheet;
-   const filterIndex = R.isNil(data.rowIndex) ? data.colIndex : data.rowIndex;
-   const newFilters = R.assoc(
-      filterIndex,
-      {
-         filterExpression: data.filterExpression,
-         caseSensitive: data.caseSensitive,
-         regex: data.regex,
-      },
-      {}
-   );
-   console.log('addNewFilter created newFilters', newFilters);
-   updatedColumnFilters(newFilters);
-   return data;
+	const filterIndex = R.isNil(data.rowIndex) ? data.colIndex : data.rowIndex;
+	const newFilters = R.assoc(
+		filterIndex,
+		{
+			filterExpression: data.filterExpression,
+			caseSensitive: data.caseSensitive,
+			regex: data.regex,
+		},
+		{}
+	);
+	R.ifElse(R.equals(ROW_AXIS), () => updatedRowFilters(newFilters), () => updatedColumnFilters(newFilters))(
+		getAxis(data)
+	);
+	return data;
 };
 
 const hideFiltered = R.pipe(
-   getDataFromActionAndStore,
-   addNewFilter,
-   R.tap(console.log),
-   filterWholeSheet
+	getDataFromActionAndStore,
+	addNewFilter,
+	filterAxes,
+	filterCells
 );
 
 export default store => next => action => {
-   if (!action) {
-      return;
-   }
-   switch (action.type) {
-      case HIDE_FILTERED:
-         hideFiltered(action.payload, store);
-         break;
-   }
-
-   return next(action);
+	if (!action) {
+		return;
+	}
+	switch (action.type) {
+		case HIDE_FILTERED:
+			hideFiltered(action.payload, store);
+			break;
+		default:
+	}
+	return next(action);
 };
