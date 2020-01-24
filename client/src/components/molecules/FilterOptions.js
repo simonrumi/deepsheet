6e869527f9d824af4b5ac0379ef3250d02253549
simonrumi <simon@dsonic.com>
2020-01-24 07:50:07 -0800

    further refactoring of filtering

--- client/src/components/molecules/FilterOptions.js
+++ client/src/components/molecules/FilterOptions.js
@@ -7,57 +7,71 @@ import Checkbox from './Checkbox';
 import Button from '../atoms/Button';
 
 class FilterOptions extends Component {
-	render() {
-		const allClasses =
-			'border-t-0 border-r border-b border-l border-solid border-grey-blue flex items-start justify-between px-2 py-2 ' +
-			this.props.classes;
+   render() {
+      const allClasses =
+         'border-t-0 border-r border-b border-l border-solid border-grey-blue flex items-start justify-between px-2 py-2 ' +
+         this.props.classes;
 
-		return (
-			<div className={allClasses}>
-				<Label label="Filter" />
-				<div>
-					<Field name="filterExpression" component={this.renderFilterInput} />
-					<div className="flex items-center px-2 py-2">
-						<Field
-							name="caseSensitiveCheckbox"
-							component={this.renderCaseSensitiveCheckbox}
-							classes="pl-0"
-						/>
-						<Label label="Case sensitive" classes="pl-2" />
-					</div>
-					<div className="flex items-center px-2 py-2">
-						<Field name="regexCheckbox" component={this.renderRegexCheckbox} />
-						<Label label="Regular expression" classes="pl-2" />
-					</div>
-					<Button
-						buttonType="button"
-						classes=""
-						onClickFn={() => alert('TODO: clear filtering')}
-						label="Clear Filtering"
-					/>
-				</div>
-			</div>
-		);
-	}
+      // TODO: the clear all filtering button should dispatch an action to update column & row visibility to be empty objects
 
-	renderFilterInput = formProps => {
-		return <TextInput formProps={formProps} testId="filterInput" placeholder="placeholder value here" />;
-	};
+      return (
+         <div className={allClasses}>
+            <Label label="Filter" />
+            <div>
+               <Field
+                  name="filterExpression"
+                  component={this.renderFilterInput}
+               />
+               <div className="flex items-center px-2 py-2">
+                  <Field
+                     name="caseSensitiveCheckbox"
+                     component={this.renderCaseSensitiveCheckbox}
+                     classes="pl-0"
+                  />
+                  <Label label="Case sensitive" classes="pl-2" />
+               </div>
+               <div className="flex items-center px-2 py-2">
+                  <Field
+                     name="regexCheckbox"
+                     component={this.renderRegexCheckbox}
+                  />
+                  <Label label="Regular expression" classes="pl-2" />
+               </div>
+               <Button
+                  buttonType="button"
+                  classes=""
+                  onClickFn={() => alert('TODO: clear filtering')}
+                  label="Clear All Filtering"
+               />
+            </div>
+         </div>
+      );
+   }
 
-	renderCaseSensitiveCheckbox = formProps => {
-		return <Checkbox formProps={formProps} testId="caseSensitiveCheckbox" />;
-	};
+   renderFilterInput = formProps => {
+      return (
+         <TextInput
+            formProps={formProps}
+            testId="filterInput"
+            placeholder="placeholder value here"
+         />
+      );
+   };
 
-	renderRegexCheckbox = formProps => {
-		return <Checkbox formProps={formProps} testId="regexCheckbox" />;
-	};
+   renderCaseSensitiveCheckbox = formProps => {
+      return <Checkbox formProps={formProps} testId="caseSensitiveCheckbox" />;
+   };
+
+   renderRegexCheckbox = formProps => {
+      return <Checkbox formProps={formProps} testId="regexCheckbox" />;
+   };
 }
 
 const mapStateToProps = (state, ownProps) => {
-	return {
-		sheet: state.sheet, // probably don't need this
-		classes: ownProps.classes || '',
-	};
+   return {
+      sheet: state.sheet, // probably don't need this
+      classes: ownProps.classes || '',
+   };
 };
 
 export default connect(mapStateToProps)(FilterOptions);
