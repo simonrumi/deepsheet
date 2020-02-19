c8fc653c0cb4b8a6460fe38d3f7b9fa3f11509b0
simonrumi <simon@dsonic.com>
2020-02-18 18:49:58 -0800

    drag and drop for rows still in development

--- client/src/components/molecules/RowHeaderDetail.js
+++ client/src/components/molecules/RowHeaderDetail.js
@@ -8,77 +8,86 @@ import { toggledShowFilterModal } from '../../actions';
 import IconFilter from '../atoms/IconFilter';
 
 class RowHeaderDetail extends Component {
-	constructor(props) {
-		super(props);
-		this.showFilterModalForRow = this.showFilterModalForRow.bind(this);
-		this.isFilterEngaged = this.isFilterEngaged.bind(this);
-	}
+   constructor(props) {
+      super(props);
+      this.showFilterModalForRow = this.showFilterModalForRow.bind(this);
+      this.isFilterEngaged = this.isFilterEngaged.bind(this);
+   }
 
-	showFilterModalForRow = rowIndex => this.props.toggledShowFilterModal(rowIndex, null);
+   showFilterModalForRow = rowIndex =>
+      this.props.toggledShowFilterModal(rowIndex, null);
 
-	isFilterEngaged = rowIndex => {
-		if (R.hasPath([rowIndex, 'filterExpression'], this.props.rowFilters)) {
-			return R.not(R.isEmpty(this.props.rowFilters[rowIndex].filterExpression));
-		}
-		return false;
-	};
+   isFilterEngaged = rowIndex => {
+      if (R.hasPath([rowIndex, 'filterExpression'], this.props.rowFilters)) {
+         return R.not(
+            R.isEmpty(this.props.rowFilters[rowIndex].filterExpression)
+         );
+      }
+      return false;
+   };
 
-	render() {
-		const { row } = extractRowColFromCellKey(this.props.cellKey);
-		const rowNum = indexToRowNumber(row);
+   render() {
+      const { row } = extractRowColFromCellKey(this.props.cellKey);
+      const rowNum = indexToRowNumber(row);
 
-		// These props are injected by React DnD,
-		// as defined by your `collect` function above:
-		const { isDragging, connectDragSource } = this.props;
+      // These props are injected by React DnD, as defined by your `collect` function above:
+      const { connectDragSource } = this.props;
 
-		return connectDragSource(
-			<div className="flex h-full cursor-row-resize">
-				<div key={'rowNum_' + row} className="w-2/4 text-center self-center text-grey-blue">
-					{rowNum}
-				</div>
-				<IconFilter
-					key={'iconFilter_' + row}
-					classes={'w-2/4 text-center'}
-					fitlerEngaged={this.isFilterEngaged(row)}
-					onClickFn={() => this.showFilterModalForRow(row)}
-					testId={'row' + rowNum}
-				/>
-			</div>
-		);
-	}
+      return connectDragSource(
+         <div className="flex w-full h-full cursor-row-resize">
+            <div
+               key={'rowNum_' + row}
+               className="w-2/4 text-center self-center text-grey-blue"
+            >
+               {rowNum}
+            </div>
+            <IconFilter
+               key={'iconFilter_' + row}
+               classes={'w-2/4 text-center'}
+               fitlerEngaged={this.isFilterEngaged(row)}
+               onClickFn={() => this.showFilterModalForRow(row)}
+               testId={'row' + rowNum}
+            />
+         </div>
+      );
+   }
 }
 
 const dragSourceSpec = {
-	beginDrag: (props, monitor, component) => {
-		console.log('beginDrag, props =', props);
-		const { row } = extractRowColFromCellKey(props.cellKey);
-		return { rowIndex: row };
-	},
-	endDrag: (props, monitor, component) => {
-		console.log('endDrag, props =', props);
-	},
+   beginDrag: (props, monitor, component) => {
+      console.log('beginDrag, props =', props);
+      const { row } = extractRowColFromCellKey(props.cellKey);
+      return { rowIndex: row };
+   },
+   endDrag: (props, monitor, component) => {
+      console.log('endDrag, props =', props);
+   },
 };
 
 const dragCollect = (connect, monitor) => {
-	return {
-		// Call this function inside render() to let React DnD handle the drag events:
-		connectDragSource: connect.dragSource(),
-		// You can ask the monitor about the current drag state:
-		isDragging: monitor.isDragging(),
-	};
+   return {
+      // Call this function inside render() to let React DnD handle the drag events:
+      connectDragSource: connect.dragSource(),
+      // You can ask the monitor about the current drag state:
+      isDragging: monitor.isDragging(),
+   };
 };
 
 function mapStateToProps(state, ownProps) {
-	return {
-		showFilterModal: state.showFilterModal,
-		cellKey: ownProps.cellKey,
-		totalRows: state.sheet.totalRows,
-		rowFilters: state.sheet.rowFilters,
-	};
+   return {
+      showFilterModal: state.showFilterModal,
+      cellKey: ownProps.cellKey,
+      totalRows: state.sheet.totalRows,
+      rowFilters: state.sheet.rowFilters,
+   };
 }
 
-const DragableRowHeader = DragSource(ItemTypes.DRAGGABLE_ROW_HEADER, dragSourceSpec, dragCollect)(RowHeaderDetail);
+const DragableRowHeader = DragSource(
+   ItemTypes.DRAGGABLE_ROW_HEADER,
+   dragSourceSpec,
+   dragCollect
+)(RowHeaderDetail);
 export default connect(
-	mapStateToProps,
-	{ toggledShowFilterModal }
+   mapStateToProps,
+   { toggledShowFilterModal }
 )(DragableRowHeader);
--- client/src/components/organisms/RowHeader.js
+++ client/src/components/organisms/RowHeader.js
@@ -6,29 +6,28 @@ import RowDropTarget from '../molecules/RowDropTarget';
 import RowHeaderDetail from '../molecules/RowHeaderDetail';
 
 class RowHeader extends Component {
-	render() {
-		const { row } = extractRowColFromCellKey(this.props.cellKey);
-		return (
-			<div className="flex flex-col justify-between h-full border-t border-l">
-				<RowHeaderDetail cellKey={this.props.cellKey} />
-				<RowDropTarget
-					key={'rowDropTarget_' + row}
-					rowIndex={row}
-					classes="w-full self-end cursor-row-resize"
-				/>
-			</div>
-		);
-		return this.renderRowHeader();
-	}
+   render() {
+      const { row } = extractRowColFromCellKey(this.props.cellKey);
+      return (
+         <div className="flex flex-col justify-between h-full border-t border-l">
+            <RowHeaderDetail cellKey={this.props.cellKey} />
+            <RowDropTarget
+               key={'rowDropTarget_' + row}
+               rowIndex={row}
+               classes="w-full self-end cursor-row-resize"
+            />
+         </div>
+      );
+   }
 }
 
 function mapStateToProps(state, ownProps) {
-	return {
-		showFilterModal: state.showFilterModal,
-		cellKey: ownProps.cellKey,
-		totalRows: state.sheet.totalRows,
-		rowFilters: state.sheet.rowFilters,
-	};
+   return {
+      showFilterModal: state.showFilterModal,
+      cellKey: ownProps.cellKey,
+      totalRows: state.sheet.totalRows,
+      rowFilters: state.sheet.rowFilters,
+   };
 }
 
 export default connect(mapStateToProps)(RowHeader);
