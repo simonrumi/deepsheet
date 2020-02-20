6e01abb3c95b07289c3f286f0be0e3606da32e1c
Simon Amarasingham <simon@dsonic.com>
2020-02-20 07:12:55 -0800

    starting to add moveRow functionality

--- client/src/components/molecules/RowDropTarget.js
+++ client/src/components/molecules/RowDropTarget.js
@@ -3,66 +3,60 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { DropTarget } from 'react-dnd';
 import { ItemTypes } from '../../constants';
+import { moveRow } from '../../services/moveRow';
 
 const targetSpec = {
-   drop: (props, monitor, component) =>
-      console.log('targetSpec.drop: props=', props),
-   // hover: (props, monitor, component) =>
-   //    console.log('targetSpec.hover: props=', props),
-   // canDrop: (props, monitor) =>
-   //    console.log('targetSpec.canDrop: props=', props),
+	drop: (props, monitor, component) => {
+		console.log('targetSpec.drop: props=', props, 'monitor', monitor, 'component', component);
+		// TODO here is where we need to  trigger an action to moveRow
+	},
+
+	// hover: (props, monitor, component) =>
+	//    console.log('targetSpec.hover: props=', props),
+	// canDrop: (props, monitor) =>
+	//    console.log('targetSpec.canDrop: props=', props),
 };
 
 const targetCollect = (connect, monitor, props) => {
-   return {
-      // Call this function inside render() to let React DnD handle the drag events:
-      connectDropTarget: connect.dropTarget(),
+	return {
+		// Call this function inside render() to let React DnD handle the drag events:
+		connectDropTarget: connect.dropTarget(),
 
-      // You can ask the monitor about the current drag state:
-      isOver: monitor.isOver(),
-      canDrop: monitor.canDrop(),
-      itemType: monitor.getItemType(),
-   };
+		// You can ask the monitor about the current drag state:
+		isOver: monitor.isOver(),
+		canDrop: monitor.canDrop(),
+		itemType: monitor.getItemType(),
+	};
 };
 
 const renderClasses = (classes, isOver, canDrop) =>
-   R.cond([
-      [
-         R.thunkify(R.and)(isOver, canDrop),
-         () => classes + ' h-1 bg-vibrant-blue visible',
-      ],
-      [
-         R.thunkify(R.and)(!isOver, canDrop),
-         () => classes + ' h-1 bg-light-light-blue visible',
-      ],
-      [R.thunkify(R.and)(!isOver, !canDrop), () => classes + ' h-1 invisible'],
-   ])();
+	R.cond([
+		[R.thunkify(R.and)(isOver, canDrop), () => classes + ' h-1 bg-vibrant-blue visible'],
+		[R.thunkify(R.and)(!isOver, canDrop), () => classes + ' h-1 bg-light-light-blue visible'],
+		[R.thunkify(R.and)(!isOver, !canDrop), () => classes + ' h-1 invisible'],
+	])();
 
 class RowDropTarget extends Component {
-   render() {
-      const id = 'rowTarget_' + this.props.rowIndex;
+	render() {
+		const id = 'rowTarget_' + this.props.rowIndex;
 
-      // These props are injected by React DnD, as defined by targetCollect function above:
-      const { isOver, canDrop, connectDropTarget } = this.props;
+		// These props are injected by React DnD, as defined by targetCollect function above:
+		const { isOver, canDrop, connectDropTarget } = this.props;
 
-      const allClasses = renderClasses(this.props.classes, isOver, canDrop);
-      return connectDropTarget(<div id={id} className={allClasses} />);
-   }
+		const allClasses = renderClasses(this.props.classes, isOver, canDrop);
+		return connectDropTarget(<div id={id} className={allClasses} />);
+	}
 }
 
 function mapStateToProps(state, ownProps) {
-   return {
-      rowIndex: ownProps.rowIndex,
-      classes: ownProps.classes,
-   };
+	return {
+		rowIndex: ownProps.rowIndex,
+		classes: ownProps.classes,
+	};
 }
 
-const RowDropTargetWrapped = DropTarget(
-   ItemTypes.DRAGGABLE_ROW_HEADER,
-   targetSpec,
-   targetCollect
-)(RowDropTarget);
+const RowDropTargetWrapped = DropTarget(ItemTypes.DRAGGABLE_ROW_HEADER, targetSpec, targetCollect)(RowDropTarget);
 export default connect(
-   mapStateToProps,
-   {}
+	mapStateToProps,
+	{}
 )(RowDropTargetWrapped);
---
+++ client/src/services/moveRow.js
@@ -0,0 +1,49 @@
+import * as R from 'ramda';
+
+const createArrayOfNums = length => {
+	const arr = new Array(length);
+	for (let i = 0; i < length; i++) {
+		arr[i] = i;
+	}
+	return arr;
+};
+
+const mapOldIndicesToNewIndicies = (rowIndexToMove, insertAfterIndex, totalRows) => {
+	const initialArray = createArrayOfNums(totalRows);
+	if (rowIndexToMove < insertAfterIndex) {
+		const newFirstPart = R.slice(0, rowIndexToMove, initialArray); // less than rowIndexToMove is untouched
+		const newSecondPart = R.slice(rowIndexToMove + 1, insertAfterIndex + 1, initialArray); // from rowIndexToMove to insertIndex will be moved 1 closer to start
+		// rowIndexToMove goes here
+		const newEndPart = R.slice(insertAfterIndex + 1, totalRows, initialArray); // greater than insertIndex is untouched
+		return R.pipe(
+			R.concat,
+			R.concat(R.__, [rowIndexToMove]),
+			R.concat(R.__, newEndPart)
+		)(newFirstPart, newSecondPart);
+	}
+
+	if (rowIndexToMove > insertAfterIndex) {
+		const newFirstPart = R.slice(0, insertAfterIndex + 1, initialArray); // up to insertAfterIndex is untouched
+		// rowIndexToMove goes here
+		const newThirdPart = R.slice(insertAfterIndex + 1, rowIndexToMove, initialArray); // from insertAfterIndex + 1 to rowIndexToMove - 1 will be moved 1 closer to end
+		const newEndPart = R.slice(rowIndexToMove + 1, totalRows, initialArray); // greater than rowIndexToMove is untouched
+		return R.pipe(
+			R.concat,
+			R.concat(R.__, newThirdPart),
+			R.concat(R.__, newEndPart)
+		)(newFirstPart, [rowIndexToMove]);
+	}
+};
+
+const moveRowContent = (rowIndexToMove, insertBelowIndex, sheet) => {
+	const rowUpdateArr = mapOldIndicesToNewIndicies(rowIndexToMove, insertBelowIndex, sheet.totalRows);
+	return rowUpdateArr; /// for a first test just console log out the array of new indices
+};
+
+const moveRow = (rowIndexToMove, insertBelowIndex, sheet) => {
+	if (rowIndexToMove === insertBelowIndex) {
+		return null;
+	}
+	return moveRowContent(rowIndexToMove, insertBelowIndex, sheet); /// for a first test just console log out the array of new indices
+	// TODO updateVisibility(sheet); // probably need to recalculate whole sheet visibility
+};
