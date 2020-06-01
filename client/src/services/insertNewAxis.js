19bf317bb74251189ffe5c2c3aaf4a312d2544e4
simonrumi <simon@dsonic.com>
2020-06-01 07:54:12 -0700

    adding rows & columns working again

--- client/src/services/insertNewAxis.js
+++ client/src/services/insertNewAxis.js
@@ -2,37 +2,39 @@ import * as R from 'ramda';
 import managedStore from '../store';
 import { cellReducerFactory } from '../reducers/cellReducers';
 import { updatedCell } from '../actions';
+import { isSomething } from '../helpers';
 
 // returns copy of cellReducers with and added cellReducer
 export const addOneCellReducer = (cellKey, row, column, cellReducers = {}) =>
-	R.pipe(
-		cellReducerFactory,
-		R.assoc(cellKey, R.__, cellReducers)
-	)(row, column);
+   R.pipe(cellReducerFactory, R.assoc(cellKey, R.__, cellReducers))(
+      row,
+      column
+   );
 
-export const addNewCellsToStore = cells => R.map(cell => updatedCell(cell), cells);
+export const addNewCellsToStore = (cells) =>
+   R.map((cell) => updatedCell(cell), cells);
 
-export const addManyCellReducersToStore = cellReducers => {
-	const combineNewReducers = managedStore.store.reducerManager.addMany(cellReducers);
-	managedStore.store.replaceReducer(combineNewReducers);
+export const addManyCellReducersToStore = (cellReducers) => {
+   const combineNewReducers = managedStore.store.reducerManager.addMany(
+      cellReducers
+   );
+   managedStore.store.replaceReducer(combineNewReducers);
 };
 
-export const maybeAddAxisVisibilityEntry = (axisIndex, axisVisibilityObj, updatedAxisVisibilityFn) =>
-	R.when(
-		R.both(
-			// axisVisibilityObj is not empty and...
-			R.pipe(
-				R.isEmpty,
-				R.not
-			),
-			// axisVisibilityObj doesn't have an entry for the Item we're adding
-			R.pipe(
-				R.has(axisIndex),
-				R.not
-			)
-		),
-		R.pipe(
-			R.thunkify(R.assoc)(axisIndex, true, {}), // make an object like {3: true} (where 3 is the axisIndex)
-			updatedAxisVisibilityFn // add that object into the rowVisibility object
-		)
-	)(axisVisibilityObj);
+export const maybeAddAxisVisibilityEntry = (
+   axisIndex,
+   axisVisibilityObj,
+   updatedAxisVisibilityFn
+) =>
+   R.when(
+      R.both(
+         // axisVisibilityObj is not empty and...
+         R.pipe(isSomething), // was (R.isEmpty, R.not),
+         // axisVisibilityObj doesn't have an entry for the Item we're adding
+         R.pipe(R.has(axisIndex), R.not)
+      ),
+      R.pipe(
+         () => ({ index: axisIndex, isVisible: true }),
+         updatedAxisVisibilityFn // add the above object into the rowVisibility object
+      )
+   )(axisVisibilityObj);
--- client/src/services/insertNewRow.js
+++ client/src/services/insertNewRow.js
@@ -5,66 +5,80 @@
  * seem worthwhile generalizing them further.
  **/
 import * as R from 'ramda';
-import { updatedTotalRows, updatedCellKeys, updatedRowVisibility, updatedHasChanged } from '../actions';
+import {
+   updatedTotalRows,
+   updatedCellKeys,
+   updatedRowVisibility,
+   updatedHasChanged,
+} from '../actions';
 import { createCellKey } from '../helpers/cellHelpers';
 import { shouldShowColumn } from '../helpers/visibilityHelpers';
 import {
-	addOneCellReducer,
-	addNewCellsToStore,
-	addManyCellReducersToStore,
-	maybeAddAxisVisibilityEntry,
+   addOneCellReducer,
+   addNewCellsToStore,
+   addManyCellReducersToStore,
+   maybeAddAxisVisibilityEntry,
 } from './insertNewAxis';
 
 const makeNewCell = (rowIndex, columnIndex, columnVisibility) => {
-	return {
-		row: rowIndex,
-		column: columnIndex,
-		content: '',
-		visible: shouldShowColumn(columnVisibility, columnIndex),
-	};
+   return {
+      row: rowIndex,
+      column: columnIndex,
+      content: '',
+      visible: shouldShowColumn(columnVisibility, columnIndex),
+   };
 };
 
 const addOneCell = (rowIndex, columnIndex, columnVisibility, updates) => {
-	const cellKey = createCellKey(rowIndex, columnIndex);
-	const cellKeys = R.append(cellKey, updates.cellKeys);
-	const cellReducers = addOneCellReducer(cellKey, rowIndex, columnIndex, updates.cellReducers);
-	const cell = makeNewCell(rowIndex, columnIndex, columnVisibility);
-	const cells = R.append(cell, updates.cells);
-	return { cellReducers, cellKeys, cells };
+   const cellKey = createCellKey(rowIndex, columnIndex);
+   const cellKeys = R.append(cellKey, updates.cellKeys);
+   const cellReducers = addOneCellReducer(
+      cellKey,
+      rowIndex,
+      columnIndex,
+      updates.cellReducers
+   );
+   const cell = makeNewCell(rowIndex, columnIndex, columnVisibility);
+   const cells = R.append(cell, updates.cells);
+   return { cellReducers, cellKeys, cells };
 };
 
 const createUpdatesForNewCells = (
-	updates, //contains { cellReducers, cellKeys, cells }
-	columnVisibility,
-	rowIndex,
-	totalColumns,
-	columnIndex = 0
+   updates, //contains { cellReducers, cellKeys, cells }
+   columnVisibility,
+   rowIndex,
+   totalColumns,
+   columnIndex = 0
 ) => {
-	if (totalColumns === columnIndex) {
-		return updates;
-	}
-	return createUpdatesForNewCells(
-		addOneCell(rowIndex, columnIndex, columnVisibility, updates),
-		columnVisibility,
-		rowIndex,
-		totalColumns,
-		columnIndex + 1
-	);
+   if (totalColumns === columnIndex) {
+      return updates;
+   }
+   return createUpdatesForNewCells(
+      addOneCell(rowIndex, columnIndex, columnVisibility, updates),
+      columnVisibility,
+      rowIndex,
+      totalColumns,
+      columnIndex + 1
+   );
 };
 
 const insertNewRow = (cellKeys, totalRows, totalColumns, sheet) => {
-	const updates = createUpdatesForNewCells(
-		{ cellKeys: cellKeys, cellReducers: {}, cells: [] },
-		sheet.columnVisibility,
-		totalRows,
-		totalColumns
-	); // totalRows, being the count of existing rows, will give us the index of the next row
-	updatedCellKeys(updates.cellKeys);
-	addManyCellReducersToStore(updates.cellReducers);
-	maybeAddAxisVisibilityEntry(totalRows, sheet.rowVisibility, updatedRowVisibility);
-	addNewCellsToStore(updates.cells);
-	updatedTotalRows(totalRows + 1);
-	updatedHasChanged(true);
+   const updates = createUpdatesForNewCells(
+      { cellKeys: cellKeys, cellReducers: {}, cells: [] },
+      sheet.columnVisibility,
+      totalRows,
+      totalColumns
+   ); // totalRows, being the count of existing rows, will give us the index of the next row
+   updatedCellKeys(updates.cellKeys);
+   addManyCellReducersToStore(updates.cellReducers);
+   maybeAddAxisVisibilityEntry(
+      totalRows,
+      sheet.rowVisibility,
+      updatedRowVisibility
+   );
+   addNewCellsToStore(updates.cells);
+   updatedTotalRows(totalRows + 1);
+   updatedHasChanged(true);
 };
 
 export default insertNewRow;
