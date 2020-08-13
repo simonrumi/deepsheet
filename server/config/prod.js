bb8e064acb5faaff578ce3d17e1b36e9ae3286e0
simonrumi <simon@dsonic.com>
2020-08-12 19:00:22 -0700

    added reference to prod env variables

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,1 +1,5 @@
-module.exports = {};
+module.exports = {
+   mongoURI: process.env.MONGO_URI,
+   options: { dbName: process.env.DB_NAME, useNewUrlParser: true, useUnifiedTopology: true },
+   // whitelist: process.env.WHITELIST, // do we need this?
+};
