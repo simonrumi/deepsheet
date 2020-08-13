036a5b28d7683078fef5c38c92ce0e834478b545
simonrumi <simon@dsonic.com>
2020-08-13 08:25:49 -0700

    adding the WHITELIST env variable for prod

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
    mongoURI: process.env.MONGO_URI,
    options: { dbName: process.env.DB_NAME, useNewUrlParser: true, useUnifiedTopology: true },
-   // whitelist: process.env.WHITELIST, // do we need this?
+   whitelist: process.env.WHITELIST.split(','),
 };
