817a7689e85678fc7c2a3fe135124f2bafd7d23d
simonrumi <simon@dsonic.com>
2020-08-18 17:12:39 -0700

    console log in the lambda function

--- server/lambdaFunctions/hello.js
+++ server/lambdaFunctions/hello.js
@@ -1,4 +1,5 @@
 export async function handler(event, context) {
+   console.log('hello world lambda function');
    return {
       statusCode: 200,
       body: JSON.stringify({ message: 'Hello world' }),
