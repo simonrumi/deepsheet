b22f58fb17cd8384681f966b7fb051a7a3cb35c6
simonrumi <simon@dsonic.com>
2020-09-05 14:49:39 -0700

    adding prod keys for facebook

--- serverLambda/config/prod.js
+++ serverLambda/config/prod.js
@@ -2,4 +2,7 @@ module.exports = {
    mongoURI: process.env.MONGO_URI,
    options: { dbName: process.env.DB_NAME, useNewUrlParser: true, useUnifiedTopology: true },
    whitelist: process.env.WHITELIST.split(','),
+   facebookClientID: process.env.FACEBOOK_CLIENT_ID,
+   facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+   facebookStateCheck: process.env.FACEBOOK_STATE_CHECK,
 };
