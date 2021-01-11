5a62bd606c8ef6996e41a438f340dbb5beacf365
simonrumi <simon@dsonic.com>
2021-01-11 07:56:32 -0800

    minor bug fix

--- serverLambda/lambdaFunctions/auth.js
+++ serverLambda/lambdaFunctions/auth.js
@@ -30,7 +30,7 @@ export async function handler(event, context, callback) {
 
       case 'google':
          try {
-            console.log('auth.js before makeGoogleAuthCall got stateCheckValue', (stateCheck.stateCheckValue);
+            console.log('auth.js before makeGoogleAuthCall got stateCheckValue', stateCheck.stateCheckValue);
             const googleResponse = await makeGoogleAuthCall(stateCheck.stateCheckValue);
             console.log('auth.js after makeGoogleAuthCall got googleResponse', googleResponse);
             return googleResponse;
