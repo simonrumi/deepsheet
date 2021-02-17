3d49dc2451abf102fd20496f2d95c081119097c3
simonrumi <simon@dsonic.com>
2021-02-17 08:20:02 -0800

    tidy up of reducerManager

--- client/src/reducers/reducerManager.js
+++ client/src/reducers/reducerManager.js
@@ -3,84 +3,39 @@ import * as R from 'ramda';
 import { combineReducers } from 'redux';
 import undoReducer from './undoReducer';
 
-/// TODO a bunch of redundant/unnecessary updating of reducers going on here....clean up
-
-export function createReducerManager(initialReducers) {
+const createReducerManager = initialReducers => {
+   // need to maintain a separate list of the unwrapped reducers for removeMany() below
    let _reducers = { ...initialReducers };
 
    // Note that a reducer expects arguments state & action [state ~= accumulator, action ~= value]
    // and returns an updated state.
    // combineReducers takes all the reducers and returns a function that also expects state & action
    // and that function gives all the reducers a chance to update their slice of the state
-   // so here the _reducersCombined function, returned from combineReducers, acts like a reducer itself - in fact it is the root reducer
-   
    const wrapAndCombineReducers = reducers => R.compose(undoReducer, combineReducers)(reducers);
+   let _wrappedReducers = wrapAndCombineReducers(_reducers); // _wrappedReducers is now the root reducer
+   const reduce = (state, action) => _wrappedReducers(state, action); // ...so calling reduce operates on _wrappedReducers
 
-   // let _reducersCombined = combineReducers(_reducers);
-   let _reducersCombined = wrapAndCombineReducers(_reducers);
-   let keysToRemove = []; // a list of keys for reducers that have been removed
-
-   // this now becomes the root reducer
-   // it returns the combined (ie root) reducer after having added/removed any new sub-reducers
-   const reduce = (state, action) => {
-      // make sure there are no reducers to be removed first
-      let cleanedState = { ...state };
-      if (keysToRemove.length > 0) {
-         for (let key of keysToRemove) {
-            delete cleanedState[key];
-         }
-         keysToRemove = [];
-      }
-      // this delegates the store.reduce() function to the
-      // _reducersCombined. Presumably the store calls reduce() for all actions
-      return _reducersCombined(cleanedState, action);
-   };
-
-   const add = (key, newReducer) => {
-      if (!key || _reducers[key]) {
-         return;
-      }
-      _reducers[key] = newReducer;
-      // _reducersCombined = combineReducers(_reducers);
-      _reducersCombined = wrapAndCombineReducers(_reducers);
-      return _reducersCombined;
-   };
-
+   // used by initializeSheet.js
    const addMany = newReducers => {
       if (!(newReducers instanceof Object)) {
          console.warn('WARNING: addMany() did not receive any new reducers');
-         // return combineReducers(_reducers);
          return wrapAndCombineReducers(_reducers);
       }
       const allReducers = {
          ..._reducers,
          ...newReducers,
       };
-      // _reducersCombined = combineReducers(allReducers);
-      _reducersCombined = wrapAndCombineReducers(allReducers);
       _reducers = allReducers;
-      // return combineReducers(allReducers);
-      return wrapAndCombineReducers(allReducers);
+      _wrappedReducers = wrapAndCombineReducers(allReducers);
+      return _wrappedReducers;
    };
 
-   const remove = key => {
-      if (!key || !_reducers[key]) {
-         return;
-      }
-      delete _reducers[key];
-      keysToRemove.push(key);
-
-      _reducersCombined = wrapAndCombineReducers(_reducers);
-      return _reducersCombined;
-   };
-
+   // using this when loading a new sheet, to get rid of the cell reducers for the old sheet
    const removeMany = keys => {
       if (!keys || !(keys instanceof Array)) {
          console.warn('WARNING: invalid keys array supplied to reducerManager.removeMany(), so no reducers removed');
          return wrapAndCombineReducers(_reducers);
       }
-      keysToRemove = R.concat(keysToRemove, keys);
-
       const filteredReducerKeys = R.filter(reducerKey => {
          if (R.includes(reducerKey, keys)) {
             return false;
@@ -88,17 +43,17 @@ export function createReducerManager(initialReducers) {
          return true;
       }, Object.keys(_reducers));
       const remainingReducers = R.pick(filteredReducerKeys, _reducers);
-      _reducersCombined = wrapAndCombineReducers(remainingReducers);
       _reducers = remainingReducers;
-      return _reducersCombined;
+      _wrappedReducers = wrapAndCombineReducers(remainingReducers);
+      return _wrappedReducers;
    };
 
    return {
       getReducerMap: () => _reducers,
       reduce,
-      add,
       addMany,
-      remove,
       removeMany,
    };
 }
+
+export { createReducerManager };
--- client/src/services/insertNewAxis.js
+++ client/src/services/insertNewAxis.js
@@ -1,5 +1,4 @@
 import * as R from 'ramda';
-import managedStore from '../store';
 import { updatedCell, hasAddedCell } from '../actions/cellActions';
 import { isSomething } from '../helpers';
 import { createCellKey } from '../helpers/cellHelpers';
@@ -12,12 +11,6 @@ export const addNewCellsToCellDbUpdates = R.map(cell => {
    hasAddedCell({ row: cell.row, column: cell.column });
    return null; // don't need to return anything, but adding this to clear a warning in the console
 });
-
-// TODO this doesn't seem to be used - you'd think it was needed when adding new rows and columns...but if not, remove it
-export const addManyCellReducersToStore = cellReducers => {
-   const combineNewReducers = managedStore.store.reducerManager.addMany(cellReducers);
-   managedStore.store.replaceReducer(combineNewReducers);
-};
 
 export const maybeAddAxisVisibilityEntry = (axisIndex, axisVisibilityObj, updatedAxisVisibilityFn) =>
    R.when(
